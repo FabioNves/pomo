1202f86257c4afaab3d498d7f4b375c984351277
Fabio <fabionevez@outlook.pt>
2025-03-16 17:51:45 -0300

    fix bug

--- server.js
+++ server.js
@@ -39,10 +39,7 @@ app.use(
 );
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
